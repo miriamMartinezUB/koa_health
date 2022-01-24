c14fb6115fde0d01000476afab62ad374c1cea6a
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2022-01-24 02:52:32 +0100

    Add all dimens of app

---
+++ lib/resources/dimens.dart
@@ -0,0 +1,14 @@
+class Dimens {
+  static const double PADDING_XSMALL = 2;
+  static const double PADDING_SMALL = 4;
+  static const double PADDING_MEDIUM = 8;
+  static const double PADDING_LARGE = 16;
+  static const double PADDING_XLARGE = 32;
+  static const double PADDING_XXLARGE = 64;
+
+  static const double ICON_SIZE_SMALL = 20;
+  static const double ICON_SIZE_MEDIUM = 35;
+
+  static const double RADIUS_SMALL = 8;
+  static const double RADIUS_LARGE = 16;
+}
